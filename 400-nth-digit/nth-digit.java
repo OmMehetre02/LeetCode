08d6144e20848ecc74145965a7ca93c63b0329a4
Om Dagduba Mehetre <ommehetrelonar@gmail.com>
2026-05-27 14:59:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (6.83%) - LeetSync

---
+++ 400-nth-digit/nth-digit.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findNthDigit(int n) {
+
+        long digit = 1;
+        long count = 9;
+        long start = 1;
+
+        // Find number length
+        while (n > digit * count) {
+            n -= digit * count;
+            digit++;
+            count *= 10;
+            start *= 10;
+        }
+
+        // Find actual number
+        start = start + (n - 1) / digit;
+
+        // Convert number to string
+        String s = Long.toString(start);
+
+        // Return digit
+        return s.charAt((n - 1) % (int)digit) - '0';
+    }
+}
