0fe8979213be555adc55f524e1c3b46308c00951
Om Dagduba Mehetre <ommehetrelonar@gmail.com>
2026-07-14 16:57:07 +0530

    Time: 266 ms (45.14%) | Memory: 47.1 MB (34.77%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
@@ -1,36 +1,54 @@
 class Solution {
-    public int[] findEvenNumbers(int[] d) {
+    public int[] findEvenNumbers(int[] digits) {
+
         HashSet<Integer> set = new HashSet<>();
-        int n=d.length;
-        for(int i=0;i<n;i++){
-            if(d[i]==0){
+        int n = digits.length;
+
+        for (int i = 0; i < n; i++) {
+
+            // First digit cannot be 0
+            if (digits[i] == 0) {
                 continue;
             }
-            for(int j=0;j<n;j++){
-                if(j==i){
+
+            for (int j = 0; j < n; j++) {
+
+                // Cannot use the same index again
+                if (j == i) {
                     continue;
                 }
-                for(int k=0;k<n;k++){
-                    if(k==j||k==i){
+
+                for (int k = 0; k < n; k++) {
+
+                    // All three indices must be different
+                    if (k == i || k == j) {
                         continue;
                     }
-                    if(d[k]%2!=0){
+
+                    // Last digit must be even
+                    if (digits[k] % 2 != 0) {
                         continue;
                     }
-                    int num=d[i]*100+d[j]*10+d[k];
-                    set.add(num);
+
+                    int number =
+                            digits[i] * 100
+                          + digits[j] * 10
+                          + digits[k];
+
+                    set.add(number);
                 }
             }
         }
-        int ans[]=new int [set.size()];
-        
-        Integer[] temp = set.toArray(new Integer[0]);
 
-        for (int i = 0; i < temp.length; i++) {
-            ans[i] = temp[i];
+        int[] answer = new int[set.size()];
+        int index = 0;
+
+        for (int number : set) {
+            answer[index++] = number;
         }
-        Arrays.sort(ans);
 
-        return ans;
+        Arrays.sort(answer);
+
+        return answer;
     }
 }
